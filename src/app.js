946a1b0c2b9a60b49932e9c02e2830a92ae24e08
muratguenduez <muratguenduez@gmail.com>
2024-07-10 00:06:52 +0200

    future/update_1

--- src/app.js
+++ src/app.js
@@ -1,8 +1,26 @@
 import express from 'express';
-import bodyParser from 'body-parser';
+import bodyParser, { urlencoded } from 'body-parser';
+import morgan from 'morgan';
+import routes from './routes';
+import { connectDB } from './database';
 
 const app = express();
 
+// Middleware setup
 app.use(bodyParser.json());
+app.use(urlencoded({ extended: true }));
+app.use(morgan('dev'));
+
+// Database connection
+connectDB();
+
+// Routes setup
+app.use('/api', routes);
+
+// Error handling middleware
+app.use((err, req, res, next) => {
+    console.error(err.stack);
+    res.status(500).send({ message: 'Internal Server Error' });
+});
 
 export default app;
